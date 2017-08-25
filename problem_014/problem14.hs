a2a537904596599459e2cbc265834045b9182067
Hoel Kervadec <hoel.kervadec@gmail.com>
2017-08-25 13:35:36 +0200

    [tmp] Work on haskell solution

--- problem_014/problem14.hs
+++ problem_014/problem14.hs
@@ -1,16 +1,16 @@
 import Data.Word
+import Data.List.Extras.Argmax
 
 solve :: Word32 -> Word32
-solve n = ssolve n 0 0
+solve n = argmax l
+    where l = map count [1..n]
 
-ssolve :: Word32 -> Word32 -> Word32 -> Word32
-ssolve 0 m _ = m
-ssolve n m m_terms = uncurry (ssolve (n - 1)) (cmp m m_terms n (count n))
-
-cmp :: Word32 -> Word32 -> Word32 -> Word32 -> (Word32, Word32)
-cmp a a_count b b_count
-    | b_count > a_count = (b, b_count)
-    | otherwise = (a, a_count)
+-- ssolve :: Word32 -> Word32 -> Word32 -> Word32
+-- ssolve m _ 0 = m
+-- ssolve m m_terms n =
+--     let n_terms = count n
+--     in if n_terms > m_terms then ssolve n n_terms (n-1)
+--                             else ssolve m m_terms (n-1)
 
 count :: Word32 -> Word32
 count 1 = 1
@@ -19,3 +19,17 @@ count n
     | otherwise = 1 + count (3 * n + 1)
 
 main = putStrLn (show (solve 1000000))
+-- module Main where
+-- import Data.List
+-- import Data.Maybe
+
+-- f :: Integer -> Integer
+-- f n | even n    = n `div` 2
+--     | otherwise = 3 * n + 1
+
+-- fs :: Integer -> [Integer]
+-- fs 1 = [1]
+-- fs n = n : fs (f n)
+
+-- main = print ((fromMaybe 0 (elemIndex (maximum x) x)) + 1)
+--     where x = map (length . fs) [1..999999]
